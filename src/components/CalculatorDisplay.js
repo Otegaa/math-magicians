f0d3c22c0be80bd7adfe8ec3194b0778e48e2259
otegaa <ekiugbotega@gmail.com>
2023-04-27 15:14:59 +0200

    Add interface implementation for numbers

--- src/components/CalculatorDisplay.js
+++ src/components/CalculatorDisplay.js
@@ -1,75 +1,145 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const CalculatorDisplay = () => (
-  <>
+const CalculatorDisplay = () => {
+  const [value, setValue] = useState(0);
+
+  const handleClick = (num) => {
+    setValue(num);
+  };
+
+  return (
     <div className="container">
       <div className="display-calc line">
-        <span>0</span>
+        <span>{value}</span>
       </div>
 
-      <button type="button" className="btn-white">
+      <button
+        type="button"
+        className="btn-white"
+        onClick={(e) => handleClick(e.target.textContent)}
+      >
         AC
       </button>
-      <button type="button" className="btn-white">
+      <button
+        type="button"
+        className="btn-white"
+        onClick={(e) => handleClick(e.target.textContent)}
+      >
         +/-
       </button>
-      <button type="button" className="btn-white">
+      <button
+        type="button"
+        className="btn-white"
+        onClick={(e) => handleClick(e.target.textContent)}
+      >
         %
       </button>
-      <button type="button" className="btn-orange">
+      <button
+        type="button"
+        className="btn-orange"
+        onClick={(e) => handleClick(e.target.textContent)}
+      >
         &#xf7;
       </button>
 
-      <button type="button" className="btn-white">
+      <button
+        type="button"
+        className="btn-white"
+        onClick={(e) => handleClick(e.target.textContent)}
+      >
         7
       </button>
-      <button type="button" className="btn-white">
+      <button
+        type="button"
+        className="btn-white"
+        onClick={(e) => handleClick(e.target.textContent)}
+      >
         8
       </button>
-      <button type="button" className="btn-white">
+      <button
+        type="button"
+        className="btn-white"
+        onClick={(e) => handleClick(e.target.textContent)}
+      >
         9
       </button>
-      <button type="button" className="btn-orange">
+      <button
+        type="button"
+        className="btn-orange"
+        onClick={(e) => handleClick(e.target.textContent)}
+      >
         x
       </button>
 
-      <button type="button" className="btn-white">
+      <button
+        type="button"
+        className="btn-white"
+        onClick={(e) => handleClick(e.target.textContent)}
+      >
         4
       </button>
-      <button type="button" className="btn-white">
+      <button
+        type="button"
+        className="btn-white"
+        onClick={(e) => handleClick(e.target.textContent)}
+      >
         5
       </button>
-      <button type="button" className="btn-white">
+      <button
+        type="button"
+        className="btn-white"
+        onClick={(e) => handleClick(e.target.textContent)}
+      >
         6
       </button>
-      <button type="button" className="btn-orange">
+      <button
+        type="button"
+        className="btn-orange"
+        onClick={(e) => handleClick(e.target.textContent)}
+      >
         -
       </button>
 
-      <button type="button" className="btn-white">
+      <button
+        type="button"
+        className="btn-white"
+        onClick={(e) => handleClick(e.target.textContent)}
+      >
         1
       </button>
-      <button type="button" className="btn-white">
+      <button
+        type="button"
+        className="btn-white"
+        onClick={(e) => handleClick(e.target.textContent)}
+      >
         2
       </button>
-      <button type="button" className="btn-white">
+      <button
+        type="button"
+        className="btn-white"
+        onClick={(e) => handleClick(e.target.textContent)}
+      >
         3
       </button>
-      <button type="button" className="btn-orange">
+      <button
+        type="button"
+        className="btn-orange"
+        onClick={(e) => handleClick(e.target.textContent)}
+      >
         +
       </button>
 
-      <button type="button" className="btn-white btn-zero">
+      <button type="button" className="btn-white btn-zero" onClick={(e) => handleClick(e.target.textContent)}>
         0
       </button>
-      <button type="button" className="btn-white">
+      <button type="button" className="btn-white" onClick={(e) => handleClick(e.target.textContent)}>
         .
       </button>
-      <button type="button" className="btn-orange">
+      <button type="button" className="btn-orange" onClick={(e) => handleClick(e.target.textContent)}>
         =
       </button>
     </div>
-  </>
-);
+  );
+};
 
 export default CalculatorDisplay;
